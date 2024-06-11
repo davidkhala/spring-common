10dd7b55847944afbe6099e9ad045cebc14603ba
David Liu <david-khala@hotmail.com>
2024-06-11 14:24:07 +0800

    publish org.davidkhala.test

---
+++ test/src/test/java/org/davidkhala/test/TestApplicationTests.java
@@ -0,0 +1,15 @@
+package org.davidkhala.test;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+    @Test
+    void contextLoads() {
+        Assertions.assertTrue(true);
+    }
+
+}
